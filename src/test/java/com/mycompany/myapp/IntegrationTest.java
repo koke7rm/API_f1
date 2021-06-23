dbf9be96af38703a7f2687ac31ba36b143f97694
Jorge <jorge_suarez_apps1oa1920@cev.com>
2021-06-23 10:43:14 +0200

    Initial version of F1_Estudio generated by JHipster-7.0.1

---
+++ src/test/java/com/mycompany/myapp/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.mycompany.myapp;
+
+import com.mycompany.myapp.F1EstudioApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = F1EstudioApp.class)
+public @interface IntegrationTest {
+}
